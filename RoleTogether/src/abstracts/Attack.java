32de42ee253bfae42ff0c8612b7fc8fb55281782
Liam Kikuchi <liamshinx@gmail.com>
2017-11-30 08:11:53 -0800

    Abstract attack class completed

--- RoleTogether/src/abstracts/Attack.java
+++ RoleTogether/src/abstracts/Attack.java
@@ -5,13 +5,33 @@ import main.Game;
 //Attacks within batttle
 public abstract class Attack {
   
-  protected int mod;        //The amount a stat is modified by
-  protected int statMod;        //which stat is modified: str = 1, intl = 2, def = 3, res = 4, spd = 5
-  proteted boolean isStatEffect;      //Does it apply a stat effect
-  protected int effectApplied;        // what status is applied: poison = 1...
+  protected int mod;        //The amount a stat is modified for an attack
+  protected int stat;        //which stat is modified: str = 1, intl = 2, def = 3, res = 4, spd = 5
+  protected int statDebuff;       //Long-term debuff to an enemy's specified stat: look up for key
+  protected int effectApplied;        // what status is applied: insta-kill = 0, poison = 1
+  protected int hitRate;          // how likely is it to hit
   
-  public Attack(int statMod, int mod, boolean isStatEffect, int effectApplied){
+  protected Attack attack;
   
+  public Attack(int stat, int mod, int statDebuff, int effectApplied, int hitRate,){
+    this.stat = stat;
+    this.mod = mod;
+    this.statDebuff = statDebuff;
+    this.effectApplied = effectApplied;
+    this.hitRate = hitRate;
+    
   }
+  
+ public abstract void render(graphics g);
+  
+ public abstract void update();
+  
+ public abstract void target1(Attack attack);
+  
+ public abstract void target2(Attack attack)
+    
+ public abstract void target3(Attack attack)
+    
+ public abstract void target4(Attack attack)
   
 }
